acbe7215e361730ae710b1284f6f80f755b8cef1
Murtaza Saify <murtazasaify897@gmail.com>
2023-08-18 15:47:33 +0530

    STATE CONCEPT

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
-    let {title,description}=this.props;
+    let {title,description,url,newsId}=this.props;
     return (
       <div>
               <div class="card" style={{width: '18rem'}}>
-                  <img src="https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0816%2Fr1211512_1296x729_16%2D9.jpg" class="card-img-top" alt="..."/>
+                  <img src={url} alt="..."/>
                   <div class="card-body">
                       <h5 class="card-title">{title}</h5>
                       <p class="card-text">{description}</p>
-                      <a href="/newsdetail" class="btn btn-primary">Go somewhere</a>
+                      <a href="/newsdetail/" class="btn btn-sm btn-primary">Read More</a>
                   </div>
               </div>
       </div>
