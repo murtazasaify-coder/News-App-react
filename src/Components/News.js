de5233d1961970387fbf79ddb08c00873af732a1
Murtaza Saify <murtazasaify897@gmail.com>
2023-08-30 12:14:03 +0530

    converting class based to function based correct

--- src/Components/News.js
+++ src/Components/News.js
@@ -10,7 +10,7 @@ const News =(props)=> {
   const [loading,setLoading]=useState(true)
   const [page,setPage]=useState(1)
   const [totalResults,settotalResults]=useState(0)
-  document.title=`${this.capfirst(props.category)}-NewsMonkey`;
+  // document.title=`${this.capfirst(props.category)}-NewsMonkey`;
   
   const capfirst=(string)=> {
     return string[0].toUpperCase() +string.slice(1);
@@ -18,18 +18,18 @@ const News =(props)=> {
   
 
     const  fetchfunc= async()=>{
-      props.setProgress(10);
+      props.setProgress1(10);
       let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pagesize}`;
       setLoading(true);
       let data= await fetch(url);
       let parseddata=await data.json();
-      props.setProgress(30);
+      props.setProgress1(30);
       
       setArticles(parseddata.articles);
       settotalResults(parseddata.totalResults);
       setLoading(false);
       
-      props.setProgress(100);
+      props.setProgress1(100);
       console.log("artilcles" + articles.length +"totalrsults"+totalResults);
     }
 
